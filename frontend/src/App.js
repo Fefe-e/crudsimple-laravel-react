c9cef27456dd8b75e2e2e27423c2fe0e4eb3aeaf
Fefex <friquelme@waia.ar>
2023-07-13 16:43:44 -0300

    add show product y print

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
+import "./App.css";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //se importa el componente
-import ShowProducts from './components/ShowProducts';
-import CreateProduct from './components/CreateProduct';
-import EditProduct from './components/EditProduct';
+import ShowProducts from "./components/ShowProducts";
+import CreateProduct from "./components/CreateProduct";
+import EditProduct from "./components/EditProduct";
+import ViewProduct from "./components/ShowProduct";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={ <ShowProducts/>} />
-          <Route path='/create' element={ <CreateProduct/>} />
-          <Route path='/edit/:id' element={ <EditProduct/>} />
+          <Route path="/" element={<ShowProducts />} />
+          <Route path="/create" element={<CreateProduct />} />
+          <Route path="/edit/:id" element={<EditProduct />} />
+          <Route path="/view/:id" element={<ViewProduct />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/ShowProducts.js
+++ frontend/src/components/ShowProducts.js
@@ -1,64 +1,75 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-const endpoint = 'http://127.0.0.1:8000/api/v1'
+const endpoint = "http://127.0.0.1:8000/api/v1";
 const ShowProducts = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getAllProducts();
+  }, []);
 
-    const [products, setProducts] = useState([])
-    useEffect ( ()=> {
-        getAllProducts()
-    }, [])
+  const getAllProducts = async () => {
+    const response = await axios.get(`${endpoint}/products`);
+    console.log("Products");
+    console.log(response);
+    setProducts(response.data.data);
+  };
 
-    const getAllProducts = async () => {
-
-        const response = await axios.get(`${endpoint}/products`)
-        console.log('Products')
-        console.log(response)
-        setProducts(response.data.data)
-    }
-
-    const deleteProduct = async (id) => {
-
-       await axios.delete(`${endpoint}/product/${id}`)
-       getAllProducts()
-         
-    }
+  const deleteProduct = async (id) => {
+    await axios.delete(`${endpoint}/product/${id}`);
+    getAllProducts();
+  };
 
   return (
     <div>
-        <div className='d-grid gap-2'>
-            <Link to="/create" className='btn btn-success btn-lg mt-2 mb-2 text-white'>Create</Link>
-        </div>
-        <table className='table table-striped'>
-            <thead className='bg-primary text-white'>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Measurement unit</th>
-                    <th>Status</th>
-                    <th></th>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                { products.map( (product) => (
-                    <tr key={product.id}>
-                        <td>{product.name}</td>
-                        <td>{product.description}</td>
-                        <td>{product.measurement_unit}</td>
-                        <td>{product.status}</td>
-                       
-                        <td>
-                            <Link to={`/edit/${product.id}`} className='btn btn-info'>Edit</Link>
-                            <button onClick={ ()=>deleteProduct(product.id)} className='btn btn-danger'>Delete</button>
-                        </td>
-                    </tr>
-                ))}                
-            </tbody>
-        </table>
-    </div>
-  )
-}
+      <div className="d-grid gap-2">
+        <Link
+          to="/create"
+          className="btn btn-success btn-lg mt-2 mb-2 text-white"
+        >
+          Create
+        </Link>
+      </div>
 
-export default ShowProducts
+      <table className="table table-striped">
+        <thead className="bg-primary text-white">
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Measurement unit</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.description}</td>
+              <td>{product.measurement_unit}</td>
+              <td>{product.status}</td>
+
+              <td>
+                <Link to={`/view/${product.id}`} className="btn btn-success">
+                  ver
+                </Link>{" "}
+                <Link to={`/edit/${product.id}`} className="btn btn-info">
+                  Edit
+                </Link>{" "}
+                <button
+                  onClick={() => deleteProduct(product.id)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ShowProducts;
